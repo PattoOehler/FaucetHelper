8ad8bcafd6ea75433a391212bddb6cfd30b6dcfa
PattoOehler <61951735+PattoOehler@users.noreply.github.com>
2020-10-26 20:08:51 +0000

    Update Keygrabber.java

--- src/faucet/Keygrabber.java
+++ src/faucet/Keygrabber.java
@@ -43,10 +43,6 @@ public class Keygrabber {
 
 			String str= sc.nextLine();
 			
-			//System.out.println(str);
-			
-			//System.out.println("Do Something!!");
-			//System.out.println("This is the best site: " + Faucets.site[Faucets.Get_Best_Faucet()]);
 			int h = Faucets.Get_Best_Faucet();
 			if(h != -1) {
 				Chrome.open_link(h);
@@ -54,7 +50,6 @@ public class Keygrabber {
 			else {
 				System.out.println("Error: No available sites currently(try again or add more sites)");
 			}
-			//sc.close();
 		}
 	}
 
