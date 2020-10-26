f9d668ea2ac745cb90134dd7750a980782aaaf48
PattoOehler <61951735+PattoOehler@users.noreply.github.com>
2020-10-26 20:10:20 +0000

    Update Main.java

--- src/faucet/Main.java
+++ src/faucet/Main.java
@@ -9,25 +9,12 @@ public class Main extends Thread {
 
 	public static void main(String args[]) {
 		
-		//System.out.println(System.currentTimeMillis());
-		//long asdf = System.currentTimeMillis();
-		
 		File_io Files = new File_io();
 		Files.read_options_txt();
 		Files.Read_Csv();
 		
-		//Files.getbestline();
 		Faucets.Set_Order_Of_Amount();
 		
-		//Faucets.Get_Best_Faucet();
-		//int x = 0;
-		//while(x < Faucets.Order_Of_Amount.length) {
-			//System.out.println(Faucets.Order_Of_Amount[x]);
-			//x+=1;
-		//}
-		
-		//Keygrabber Keyg = new Keygrabber();
-		//Keyg.perma_run();
 		
 		if(Keygrabber.Run_On_Timer_Time != 0) {
 			Main thread = new Main();
