bc3799c292698ded5ac591795a7c9943d72998eb
PattoOehler <61951735+PattoOehler@users.noreply.github.com>
2020-10-26 19:54:56 +0000

    Update Keygrabber.java

--- src/faucet/Keygrabber.java
+++ src/faucet/Keygrabber.java
@@ -1,21 +1,15 @@
 package faucet;
-import java.util.*;  
+import java.util.*;
+import java.util.concurrent.TimeUnit;  
 
 public class Keygrabber {
-	public void perma_run() {
-
-
+	
+	public static double Run_On_Timer_Time=0;
+	
+	
+	public static void run_on_a_timer() {
 		
-
-		Scanner sc= new Scanner(System.in);
-
-
 		while(true) {
-
-			System.out.print("Press Enter: ");
-
-			String str= sc.nextLine();
-			
 			int h = Faucets.Get_Best_Faucet();
 			if(h != -1) {
 				Chrome.open_link(h);
@@ -23,6 +17,44 @@ public class Keygrabber {
 			else {
 				System.out.println("Error: No available sites currently(try again or add more sites)");
 			}
+			
+			
+			
+			try {
+				TimeUnit.MILLISECONDS.sleep((int)(Run_On_Timer_Time*1000));
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	
+	
+	
+	public static void perma_run() {	
+
+		Scanner sc= new Scanner(System.in);
+
+
+		while(true) {
+			
+			
+			System.out.print("Press Enter: ");
+
+			String str= sc.nextLine();
+			
+			//System.out.println(str);
+			
+			//System.out.println("Do Something!!");
+			//System.out.println("This is the best site: " + Faucets.site[Faucets.Get_Best_Faucet()]);
+			int h = Faucets.Get_Best_Faucet();
+			if(h != -1) {
+				Chrome.open_link(h);
+			}
+			else {
+				System.out.println("Error: No available sites currently(try again or add more sites)");
+			}
+			//sc.close();
 		}
 	}
 
