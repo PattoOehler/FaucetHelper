fc0c3a5b0e1bd81bf709c870f32d12069d641269
PattoOehler <61951735+PattoOehler@users.noreply.github.com>
2020-10-26 19:56:00 +0000

    Update Main.java

--- src/faucet/Main.java
+++ src/faucet/Main.java
@@ -1,19 +1,41 @@
 package faucet;
 
-public class Main {
+public class Main extends Thread {
+	
+	public void run() {
+		Keygrabber.run_on_a_timer();
+	}
+	
 
 	public static void main(String args[]) {
 		
+		//System.out.println(System.currentTimeMillis());
+		//long asdf = System.currentTimeMillis();
 		
 		File_io Files = new File_io();
 		Files.read_options_txt();
 		Files.Read_Csv();
 		
+		//Files.getbestline();
 		Faucets.Set_Order_Of_Amount();
 		
-		Keygrabber Keyg = new Keygrabber();
-		Keyg.perma_run();
+		//Faucets.Get_Best_Faucet();
+		//int x = 0;
+		//while(x < Faucets.Order_Of_Amount.length) {
+			//System.out.println(Faucets.Order_Of_Amount[x]);
+			//x+=1;
+		//}
 		
+		//Keygrabber Keyg = new Keygrabber();
+		//Keyg.perma_run();
+		
+		if(Keygrabber.Run_On_Timer_Time != 0) {
+			Main thread = new Main();
+			thread.start();
+		}
+		Keygrabber.perma_run();
+		
+	
 		
 	}
 }
